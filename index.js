33a34d0128bb2ab8f37f5d8775f1932262405f5d
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-01-18 17:19:15 -0500

    recoding index.js for resubmission

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const employee = require('./lib/Employee');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,13 +10,11 @@ const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+console.log('Team generator. Please follow prompts to build your team.');
 
 const teamMembers = [];
 const idArray = [];
 
-console.log(
-    '\nTeam generator. Please follow prompts to build your team.\n'
-);
 
 function appMenu() {
     function createManager () {
@@ -170,26 +169,3 @@ function appMenu() {
 }
 
 appMenu();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
