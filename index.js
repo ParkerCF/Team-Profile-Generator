3d5428c58ac64d3c7edab0c56fa867eaf50009bb
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-01-18 21:13:18 -0500

    working on index.js

--- index.js
+++ index.js
@@ -10,162 +10,74 @@ const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-console.log('Team generator. Please follow prompts to build your team.');
+console.log('Please follow the prompts to build your Team.');
 
 const teamMembers = [];
-const idArray = [];
 
 
-function appMenu() {
-    function createManager () {
-        console.log('Begin building your team');
-        inquirer
-            .prompt([
+function builder() {
+    return inquirer.prompt([
                 {
                     type: 'input',
-                    name: 'managerName',
-                    message: "What is the manager's name?",
+                    name: 'name',
+                    message: "What is the employee's name?",
                 },
                 {
                     type: 'input',
-                    name: 'managerId',
-                    message: "What is the manager's id?"
+                    name: 'id',
+                    message: "What is the employee's id?"
 
                 },
                 {
                     type: 'input',
-                    name: 'managerEmail',
-                    message: "What is the manager's email?",
-                    },
-                {
-                    type: 'input',
-                    name: 'managerOfficeNumber',
-                    message: "What is the manager's office number?",
+                    name: 'email',
+                    message: "What is the employee's email?",
+                    
                 },
-            ])
-            .then((answers) => {
-                const manager = new Manager(
-                    answers.managerName,
-                    answers.managerId, 
-                    answers.managerEmail,
-                    answers.managerOfficeNumber
-                );
-                teamMembers.push(manager);
-                idArray.push(answers.managerId);
-                createTeam();
-            });
-    }
-
-    function createTeam() {
-        inquirer
-            .prompt([
                 {
                     type: 'list',
-                    name: 'memberChoice',
-                    message: 'Which team member is next?',
-                    choices: [
-                        'Engineer',
-                        'Intern',
-                        "No more team members to add",
-                    ],
-                },
-            ])
-            .then((userChoice) => {
-                switch (userChoice.memberChoice) {
-                    case 'Engineer':
-                        addEngineer();
-                        break;
-                    case 'Intern':
-                        addIntern();
-                        break;
-                    default:
-                        buildTeam();
-                }
-            });
-    }
-
-    function addEngineer() {
-        inquirer
-            .prompt([
-                {
-                    typoe: 'input',
-                    name: 'engineerName',
-                    message: "What is the name of the engineer?",
+                    name: `role`,
+                    message: `What is the employee's role?`,
+                    choices: ['Manager', 'Engineer', "Intern"],
                 },
                 {
                     type: 'input',
-                    name: 'engineerId',
-                    message: "What is the id of the engineer?",
+                    name: 'officeNumber',
+                    message: `What is the Manager's office number?`,
+                    when: (answers) => answers.role === "Manager",
                 },
                 {
                     type: 'input',
-                    name: 'engineerEmail',
-                    message: "What is the engineer's email address?",
+                    name: 'school',
+                    message: `Where does the intern go to school?`,
+                    when: (answers) => answers.role === 'Intern',
                 },
                 {
-                    type: 'input',
-                    name: 'engineerGithub',
-                    message: "what is the engineer's GitHub username?",
+                    type: 'confirm',
+                    name: 'addEmployee',
+                    message: 'Are there any other employees to add?'
                 },
             ])
             .then((answers) => {
-                const engineer = new Engineer(
-                    answers.engineerName,
-                    answers.engineerId,
-                    answers.engineerEmail,
-                    answers.engineerGitHub
-                );
-                teamMembers.push(engineer);
-                idArray.push(answers.engineerId);
-                createTeam();
-            });
+                let emoloyee;
+            if (answers.role === 'Manager') {
+                employee = new Manager(answers.name, ansewrs.id, answers.email, answers.officeNumer);
+            } else if (answers.role === 'Engineer') {
+                emoloyee = new Engineer(ansewrs.name, answers.id, answers.email, answers.github);
+            } else if (answers.role === 'Intern') {
+                employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+            }
+
+            emoloyees.push(employee);
+            if (answers.addEmployee) {
+                return questions();
+            } else {
+                return employees;
+            }
+        })
     }
 
-    function addIntern() {
-        inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    name: 'internName',
-                    message: "What is the intern's name?",
-                },
-                {
-                    type: 'input',
-                    name: 'internId',
-                    message: "What is the intern's id?",
-                },
-                {
-                    type: 'input',
-                    name: 'internEmail',
-                    message: "What is the intern's email?"
 
-                },
-                {
-                    type: 'input',
-                    name: 'internSchool',
-                    message: "Where does the intern go to school?",
-                },
-            ])
-            .then((answers) => {
-                const intern = new Intern (
-                    answers.internName,
-                    answers.internId,
-                    answers.internEmail,
-                    answers.internSchool
-                );
-                teamMembers.push(intern);
-                idArray.push(answers.internId);
-                createTeam();
-            });
-    }
 
-    function buildTeam() {
-        if (!fs.existsSync(DIST_DIR)) {
-            fs.mkdirSync(DIST_DIR);
-        }
-        fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
-    }
-    createManager();
-}
 
 appMenu();
